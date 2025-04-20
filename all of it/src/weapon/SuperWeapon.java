78fa08f2978a72da73cd384cbbb653f124d66647
Ali Eren Küçük <alirenkucuk@gmail.com>
2025-04-20 17:01:52 +0300

    Create SuperWeapon.java

---
+++ all of it/src/weapon/SuperWeapon.java
@@ -0,0 +1,24 @@
+package weapon;
+import java.awt.image.*;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+import mainn.*;
+
+public class SuperWeapon {
+    
+    public BufferedImage image;
+    public String name;
+    public boolean collision;
+    public int x, y;
+    public Rectangle solidArea = new Rectangle(0,0,48,48);
+    public int solidAreaDefaultX = 0;
+    public int solidAreaDefaultY = 0;
+    public int attackValue;
+    
+
+    public void draw(Graphics2D g2, GamePanel gp){
+
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+    }
+}
