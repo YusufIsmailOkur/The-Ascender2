7a672fe1969e74f901784d0a8d00703b70f51eb3
Efe Atalay <efeatalay2006@gmail.com>
2025-04-25 14:34:55 +0300

    Readded Settings

--- all of it/src/mainn/Sound.java
+++ all of it/src/mainn/Sound.java
@@ -5,37 +5,44 @@ import java.net.URL;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 
 public class Sound {
     Clip clip;
     URL soundURL[] = new URL[30];
 
-    public Sound(){
+    public Sound() {
         soundURL[0] = getClass().getResource("/res/sound/titleMusic.wav");
         soundURL[1] = getClass().getResource("/res/sound/inGameMusic.wav");
     }
 
-    public void setFile(int i){
+    public void setFile(int i) {
         try {
             AudioInputStream ais = AudioSystem.getAudioInputStream(soundURL[i]);
             clip = AudioSystem.getClip();
             clip.open(ais);
-        } 
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void play(){
+    public void play() {
         clip.start();
     }
-    
-    public void loop(){
+
+    public void loop() {
         clip.loop(Clip.LOOP_CONTINUOUSLY);
     }
 
-    public void stop(){
+    public void stop() {
         clip.stop();
     }
-}
 
+    public void setVolume(float value) {
+        if (clip != null) {
+            FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            gainControl.setValue(value * 8.0f - 80.0f);
+        }
+    }
+
+}
