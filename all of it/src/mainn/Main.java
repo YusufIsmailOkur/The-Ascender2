2bbac4044900e2af74432cba43da6c24cd3f9e49
ayberk_korkmaz <ayberkkorkmaz2626@hotmail.com>
2025-04-27 16:33:05 +0300

    exiting game from titleState and menuState is handled. Fullscreen option is partially handled

--- all of it/src/mainn/Main.java
+++ all of it/src/mainn/Main.java
@@ -1,23 +1,59 @@
 package mainn;
+
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+
 import javax.swing.JFrame;
 
 public class Main {
+   static JFrame window;
+   static Boolean isFullScreen = false;
 
-     public static void main(String[] args) {
-        JFrame window = new JFrame();
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setResizable(false);
-        window.setTitle("2D GAME");
+   public static void main(String[] args) {
+      window = new JFrame();
+      window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      window.setResizable(false);
+      window.setTitle("2D GAME");
 
-        GamePanel gamePanel = new GamePanel();
-        window.add(gamePanel);
+      GamePanel gamePanel = new GamePanel();
+      window.add(gamePanel);
 
-        window.pack();
+      window.pack();
 
-        window.setLocationRelativeTo(null);
-        window.setVisible(true);
+      window.setLocationRelativeTo(null);
+      window.setVisible(true);
 
-        gamePanel.setupGame();
-        gamePanel.startGameThread();
-     }
+      gamePanel.setupGame();
+      gamePanel.startGameThread();
+   }
+
+   public static void fullScreen() {
+      GraphicsDevice graphicsDevice = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+  
+      if (!isFullScreen){
+          //if it is not fullscreen, it goes fullscreen
+          window.dispose();
+          window.setUndecorated(true);
+          window.setResizable(false);
+          graphicsDevice.setFullScreenWindow(window);
+          window.setVisible(true);
+          isFullScreen = !isFullScreen;
+      } 
+      else{
+          //exiting fullscreen
+          graphicsDevice.setFullScreenWindow(null);
+  
+          window.dispose();
+          window.setUndecorated(false);
+          window.setResizable(true);
+          window.setSize(1000, 800); 
+         //  frame.setLocationRelativeTo(null); 
+          window.setVisible(true);
+          isFullScreen = !isFullScreen;
+      }
+  }
+
+  public static Boolean getIsFullScreen() {
+   return isFullScreen;
+   }
 }
