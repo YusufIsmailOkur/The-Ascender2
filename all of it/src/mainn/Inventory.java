e4d56af08ba1579798987aaeecf6c832683d8af0
Ali Eren Küçük <alirenkucuk@gmail.com>
2025-05-13 14:17:05 +0300

    Update Inventory.java

--- all of it/src/mainn/Inventory.java
+++ all of it/src/mainn/Inventory.java
@@ -191,7 +191,7 @@ public class Inventory extends JPanel implements ActionListener {
             }
             else
             {
-                JOptionPane("This items can not merged");
+                JOptionPane.showMessageDialog("This items can not merged");
                 firstSelected = false;
                 secondSelected = false;
             }
